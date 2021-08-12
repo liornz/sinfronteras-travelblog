d7e1860a8042fa5a916eb609e552026585fef221
Lior Nitzan <nitzanlior1968@gmail.com>
2021-08-12 13:58:58 +0200

    add subtle animation to kinks in the footer

--- components/footer/menu-items.tsx
+++ components/footer/menu-items.tsx
@@ -8,11 +8,31 @@ const MenuItems: React.FC = () => {
   return (
     <div className={styles.container}>
       <Link href="/destinations">
-        {locale === 'en-US' ? 'DESTINATIONS' : 'DESTINOS'}
+        <a
+          style={{
+            color: router.pathname === '/destinations' ? 'rgb(240,150,37)' : '',
+          }}
+        >
+          {locale === 'en-US' ? 'DESTINATIONS' : 'DESTINOS'}
+        </a>
       </Link>
-      <Link href="/shop">{locale === 'en-US' ? 'SHOP' : 'TIENDA'}</Link>
+      <Link href="/shop">
+        <a
+          style={{
+            color: router.pathname === '/shop' ? 'rgb(240,150,37)' : '',
+          }}
+        >
+          {locale === 'en-US' ? 'SHOP' : 'TIENDA'}
+        </a>
+      </Link>
       <Link href="/contact">
-        {locale === 'en-US' ? 'CONTACT' : 'CONTACTO'}
+        <a
+          style={{
+            color: router.pathname === '/contact' ? 'rgb(240,150,37)' : '',
+          }}
+        >
+          {locale === 'en-US' ? 'CONTACT' : 'CONTACTO'}
+        </a>
       </Link>
     </div>
   );
